fc4e7ea63593c0714939a19de9ac1df18a34cf7e
matszach <lkaszubowski@gmail.com>
2024-06-14 23:46:19 +0200

    scss installed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appElement = document.getElementsByClassName('App')[0];
+  expect(appElement).toBeInTheDocument();
 });
