105b99ce8e95759bedb786bc84e3e679afb53f65
matszach <lkaszubowski@gmail.com>
2024-06-16 12:42:32 +0200

    minor routing tweak

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppNavbar from './components/navbar/Nav';
 import { AppRoutes } from './const/routes.const';
 import { content } from './content/content';
@@ -9,34 +9,30 @@ import SpellsView from './components/spells/SpellsView';
 function App() {
   return (
     <div className="App">
+      <AppNavbar></AppNavbar>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={
-            <>
-              <AppNavbar></AppNavbar>
-              <Outlet />
-            </>
-          }>
-            <Route path={AppRoutes.HOME} element={
-              <div>{content.hello}</div>
-            }/>
-            <Route path={AppRoutes.SPELLS} element={<SpellsView></SpellsView>}/>
-            <Route path={AppRoutes.EQUIPMENT} element={
-              <div>{content.workInProgress}</div>
-            }/>
-            <Route path={AppRoutes.RANDOM.SPELLS} element={
-              <div>{content.workInProgress}</div>
-            }/>
-            <Route path={AppRoutes.RANDOM.LOOT} element={
-              <div>{content.workInProgress}</div>
-            }/>
-            <Route path={AppRoutes.RANDOM.SHOP} element={
-              <div>{content.workInProgress}</div>
-            }/>
-            <Route path={AppRoutes.UNKNOWN} element={
-              <div>{content.notFound}</div>
-            }/>
-          </Route>
+          <Route path={AppRoutes.HOME} element={
+            <div>{content.hello}</div>
+          }/>
+          <Route path={AppRoutes.SPELLS} element={
+            <SpellsView></SpellsView>
+          }/>
+          <Route path={AppRoutes.EQUIPMENT} element={
+            <div>{content.workInProgress}</div>
+          }/>
+          <Route path={AppRoutes.RANDOM.SPELLS} element={
+            <div>{content.workInProgress}</div>
+          }/>
+          <Route path={AppRoutes.RANDOM.LOOT} element={
+            <div>{content.workInProgress}</div>
+          }/>
+          <Route path={AppRoutes.RANDOM.SHOP} element={
+            <div>{content.workInProgress}</div>
+          }/>
+          <Route path={AppRoutes.UNKNOWN} element={
+            <div>{content.notFound}</div>
+          }/>
         </Routes>
       </BrowserRouter>
     </div>
