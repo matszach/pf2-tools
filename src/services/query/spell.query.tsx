77e5a4e33da4ca4b9a162db1be1b217e0a48230e
matszach <lkaszubowski@gmail.com>
2024-06-22 15:57:25 +0200

    lint fix

--- src/services/query/spell.query.tsx
+++ src/services/query/spell.query.tsx
@@ -13,7 +13,7 @@ export function spellQuery(
       return !tradition || tradition === 'all' || spell.traditions.includes(tradition)
     })
     .filter(spell => {
-      return !level || spell.level >= level[0] && spell.level <= level[1]
+      return !level || (spell.level >= level[0] && spell.level <= level[1])
     })
     .filter(spell => {
       if (traits) {
@@ -58,8 +58,8 @@ export function spellQuery(
       return true
     })
     .sort((s1: Spell, s2: Spell) => {
-      const s1v = s1[field as keyof Spell] ?? '';
-      const s2v = s2[field as keyof Spell] ?? '';
+      const s1v = s1[field as keyof Spell] ?? ''
+      const s2v = s2[field as keyof Spell] ?? ''
       if (s1v === s2v) {
         return 0
       }
